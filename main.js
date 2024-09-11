df3990521073597333961cf0fbafb24a765c200f
nureddinhaji <drnhaji@hotmail.com>
2024-09-11 17:04:19 +0300

    create delete and check task functions

--- main.js
+++ main.js
@@ -11,10 +11,24 @@ function addTask() {
 function createTaskElement(task) {
     const taskElement = document.createElement('div');
     taskElement.classList.add('app__task');
-    taskElement.innerHTML = `<button class="app__taskcheck" disabled><img src="./images/icon-check.svg"/></button>
-    <input type="text" name="newtask" id="newtask" class="app__newtask app__input" value="${task}" disabled>
-    <button class="app__taskdelete" disabled><img src="./images/icon-cross.svg"/></button>`;
-    taskList.insertAdjacentElement("afterbegin", taskElement)
+
+    const taskElementHtml = `<button class="app__taskcheck"><img src="./images/icon-check.svg"/></button>
+    <input type="text" class="app__input" value="${task}" disabled>
+    <button class="app__taskdelete"><img src="./images/icon-cross.svg"/></button>`;
+    
+    let fragment = document.createRange().createContextualFragment(taskElementHtml);
+
+    fragment.querySelector(".app__taskdelete").addEventListener("click", () => {
+            console.log("clicked");
+            deleteTask(taskElement);
+        });
+
+    fragment.querySelector(".app__taskcheck").addEventListener("click", () => {
+            checkTask(taskElement);
+        });
+
+    taskElement.appendChild(fragment);
+    taskList.insertAdjacentElement("afterbegin", taskElement);
 }
 
 // Event listener for the new task input
@@ -22,4 +36,16 @@ newTaskInput.addEventListener(("keydown"), (e) => {
     if (e.key === "Enter") {
         addTask();
     }
-})
+});
+
+// Function for delete the task
+function deleteTask(task) {
+    console.log(task)
+    task.remove();
+}
+
+// Function to check the task and make it complated
+function checkTask(task) {
+    task.classList.toggle("app__task--complated");
+}
+
